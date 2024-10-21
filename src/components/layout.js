b97d05100ba756887664252ee98182a608827a76
IsisAS <94575790+IsisDevCit02@users.noreply.github.com>
2024-10-21 20:48:15 -0300

    - Adicionado o titulo da página pretige vistos

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,15 +3,22 @@ import Header from "./Header/header";
 import BackgroundVideo from "../assets/videos/background.mp4";
 import "./layout.css";
 
-const Layout = ({ children }) => {
+const Layout = () => {
   return (
     <>
       <div className="container-video">
         <video autoPlay muted loop className="video-background">
           <source src={BackgroundVideo} type="video/mp4" />
         </video>
-        <div className="container-background"/>
+        <div className="container-background" />
         <Header />
+        <div className="container-title">
+          <p className="title">
+            O seu sonho de ir para os Estados Unidos está a um clique de
+            distância
+          </p>
+          <button className="button-request">Solicite seu Visto Agora</button>
+        </div>
       </div>
     </>
   );
