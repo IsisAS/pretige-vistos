dcc5bd205bde457962488fa58e00c69bbbc0c6ba
IsisAS <94575790+IsisDevCit02@users.noreply.github.com>
2024-10-24 20:49:43 -0300

    - Alterado case da pastas

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import Layout from "../../components/layout"
-import Evidences from "../evidences"
+import Evidences from "../Evidences"
 
 export default function Home(): JSX.Element {
   return (
