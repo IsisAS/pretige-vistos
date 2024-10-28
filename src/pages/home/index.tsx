c736751f4f1914d6bdbdda1fa55ba5ca21be8bef
IsisAS <144072352+IsisAS@users.noreply.github.com>
2024-10-28 19:43:06 -0300

    fix/evidences

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
 import Layout from "../../components/layout";
-import Evidences from "../Evidences";
+
 import WhoAreWe from "../WhoAreWe";
 import Services from "../Services";
 import CommonQuestion from "../CommonQuestion";
+import Evidences from "../Evidences";
 
 export default function Home(): JSX.Element {
   return (
