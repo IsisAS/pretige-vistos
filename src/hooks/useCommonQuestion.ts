08605c05b7b9b36e5a7054489a9ad927c9318c9e
IsisAS <144072352+IsisAS@users.noreply.github.com>
2024-11-24 18:54:48 -0300

    - Adicionado "." final a todas as perguntas do site

--- src/hooks/useCommonQuestion.ts
+++ src/hooks/useCommonQuestion.ts
@@ -21,17 +21,17 @@ export default function useCommonQuestion(): UseCommonQuestionReturn {
         question:
           "O que é a taxa MRV no processo de solicitação de visto americano?",
         answer:
-          "A taxa MRV (Machine Readable Visa) é a taxa de solicitação obrigatória para a maioria dos vistos não-imigrantes dos Estados Unidos, como vistos de turismo, estudo e trabalho. Essa taxa cobre os custos de processamento do pedido de visto e deve ser paga antes de agendar a entrevista no consulado ou Embaixada dos EUA",
+          "A taxa MRV (Machine Readable Visa) é a taxa de solicitação obrigatória para a maioria dos vistos não-imigrantes dos Estados Unidos, como vistos de turismo, estudo e trabalho. Essa taxa cobre os custos de processamento do pedido de visto e deve ser paga antes de agendar a entrevista no consulado ou Embaixada dos EUA.",
       },
       {
         question: "Quanto custa a taxa de solicitação do visto?",
         answer:
-          "A taxa varia de acordo com o tipo de visto. Para vistos de turismo e negócios (B1/B2), por exemplo, a taxa é de US$185. (em média R$1.036)",
+          "A taxa varia de acordo com o tipo de visto. Para vistos de turismo e negócios (B1/B2), por exemplo, a taxa é de US$185. (em média R$1.036).",
       },
       {
         question: "Preciso agendar uma entrevista para renovar meu visto?",
         answer:
-          "Em alguns casos, é possível renovar o visto até 48 meses após o vencimento sem necessidade de entrevista, conforme as regras vigentes. A renovação pode ser feita a qualquer momento, inclusive antes do visto expirar",
+          "Em alguns casos, é possível renovar o visto até 48 meses após o vencimento sem necessidade de entrevista, conforme as regras vigentes. A renovação pode ser feita a qualquer momento, inclusive antes do visto expirar.",
       },
       {
         question:
@@ -42,7 +42,7 @@ export default function useCommonQuestion(): UseCommonQuestionReturn {
       {
         question: "A agência consegue adiantar a data da entrevista?",
         answer:
-          "Sim, nós monitoramos diariamente as vagas para agendamento. Desta forma, conseguimos as datas mais próximas para nossos clientes",
+          "Sim, nós monitoramos diariamente as vagas para agendamento. Desta forma, conseguimos as datas mais próximas para nossos clientes.",
       },
     ]
 
